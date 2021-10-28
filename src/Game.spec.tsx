e6880c956e3579746d5ed0ad6e2d7b11dafbdcb9
Nathan Chancon <nathanchacon321@gmail.com>
2021-10-27 23:20:00 -0300

    Test if player X and O can win

--- src/Game.spec.tsx
+++ src/Game.spec.tsx
@@ -1,5 +1,7 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import Game from "./Game";
+
+afterEach(cleanup)
 
 it("renders game headings", () => {
   const { getByText } = render(<Game />);
@@ -18,3 +20,44 @@ it("renders board and check for step counter update", () => {
   // Expect "Current step: 1" to be found
   getByText("Current step: 1");
 });
+
+it("should player X be winner", () => {
+  
+  const { getByText, getByTestId, findByText} = render(<Game />);
+
+  const square0 = getByTestId(`square-0`);
+  const square4 = getByTestId(`square-4`);
+  const square8 = getByTestId(`square-8`);
+
+  const square1 = getByTestId(`square-1`);
+  const square2 = getByTestId(`square-2`);
+
+  fireEvent.click(square0)
+  fireEvent.click(square1)
+  fireEvent.click(square4)
+  fireEvent.click(square2)
+  fireEvent.click(square8)
+
+  getByText("Winner: X");
+})
+
+it("should player O be winner", async () => {
+  const { getByText, getByTestId } = render(<Game />);
+
+  const square0 = getByTestId(`square-0`);
+  const square1 = getByTestId(`square-1`);
+  const square2 = getByTestId(`square-2`);
+
+  const square3 = getByTestId(`square-3`);
+  const square5 = getByTestId(`square-5`);
+  const square8 = getByTestId(`square-8`);
+
+  fireEvent.click(square0)
+  fireEvent.click(square2)
+  fireEvent.click(square1)
+  fireEvent.click(square5)
+  fireEvent.click(square3)
+  fireEvent.click(square8)
+
+  getByText("Winner: O");
+})
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import useGameState from "./useGameState";
 
 function calculateWinner(squares : any) {
